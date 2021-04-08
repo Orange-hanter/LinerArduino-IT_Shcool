274e8ed8aed8a02dd18f978d64ea80aa3de21ad2
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2021-04-08 16:02:21 +0300

    Increase USB speed and remove #defile

--- Firmware.ino
+++ Firmware.ino
@@ -5,34 +5,40 @@
 
 
 // Pin Definitions
-#define DCMOTORDRIVERL298_PIN_INT1	2
-#define DCMOTORDRIVERL298_PIN_ENB	6
-#define DCMOTORDRIVERL298_PIN_INT2	3
-#define DCMOTORDRIVERL298_PIN_ENA	5
-#define DCMOTORDRIVERL298_PIN_INT3	4
-#define DCMOTORDRIVERL298_PIN_INT4	7
-#define IRLINEFOLLOW_1_PIN_OUT	8
-#define IRLINEFOLLOW_2_PIN_OUT	9
-
+const int DCMOTORDRIVERL298_PIN_INT1 = 2;
+const int DCMOTORDRIVERL298_PIN_ENB = 6;
+const int DCMOTORDRIVERL298_PIN_INT2 = 3;
+const int DCMOTORDRIVERL298_PIN_ENA = 5;
+const int DCMOTORDRIVERL298_PIN_INT3 = 4;
+const int DCMOTORDRIVERL298_PIN_INT4 = 7;
+const int IRLINEFOLLOW_1_PIN_OUT = 8;
+const int IRLINEFOLLOW_2_PIN_OUT = 9;
 
 
 // Global variables and defines
-
-// object initialization
-DCMDriverL298 dcMotorDriverL298(DCMOTORDRIVERL298_PIN_ENA,DCMOTORDRIVERL298_PIN_INT1,DCMOTORDRIVERL298_PIN_INT2,DCMOTORDRIVERL298_PIN_ENB,DCMOTORDRIVERL298_PIN_INT3,DCMOTORDRIVERL298_PIN_INT4);
-
-
-// define vars for testing menu
-const int timeout = 10000;       //define timeout of 10 sec
+const int timeout = 10000;       // timeout of 10 sec
 char menuOption = 0;
 long time0;
+
+char menu();
+
+// object initialization
+DCMDriverL298 dcMotorDriverL298(DCMOTORDRIVERL298_PIN_ENA,
+                                DCMOTORDRIVERL298_PIN_INT1,
+                                DCMOTORDRIVERL298_PIN_INT2,
+                                DCMOTORDRIVERL298_PIN_ENB,
+                                DCMOTORDRIVERL298_PIN_INT3,
+                                DCMOTORDRIVERL298_PIN_INT4);
+
+
+
 
 // Setup the essentials for your circuit to work. It runs first every time your circuit is powered with electricity.
 void setup() 
 {
     // Setup Serial which is useful for debugging
     // Use the Serial Monitor to view printed messages
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial) ; // wait for serial port to connect. Needed for native USB
     Serial.println("start");
     
@@ -119,46 +125,3 @@ char menu()
     }
 }
 
-/*******************************************************
-
-*    Circuito.io is an automatic generator of schematics and code for off
-*    the shelf hardware combinations.
-
-*    Copyright (C) 2016 Roboplan Technologies Ltd.
-
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*    In addition, and without limitation, to the disclaimers of warranties 
-*    stated above and in the GNU General Public License version 3 (or any 
-*    later version), Roboplan Technologies Ltd. ("Roboplan") offers this 
-*    program subject to the following warranty disclaimers and by using 
-*    this program you acknowledge and agree to the following:
-*    THIS PROGRAM IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS, AND 
-*    WITHOUT WARRANTIES OF ANY KIND EITHER EXPRESS OR IMPLIED.  ROBOPLAN 
-*    HEREBY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT 
-*    NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, TITLE, FITNESS 
-*    FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND THOSE ARISING BY 
-*    STATUTE OR FROM A COURSE OF DEALING OR USAGE OF TRADE. 
-*    YOUR RELIANCE ON, OR USE OF THIS PROGRAM IS AT YOUR SOLE RISK.
-*    ROBOPLAN DOES NOT GUARANTEE THAT THE PROGRAM WILL BE FREE OF, OR NOT 
-*    SUSCEPTIBLE TO, BUGS, SECURITY BREACHES, OR VIRUSES. ROBOPLAN DOES 
-*    NOT WARRANT THAT YOUR USE OF THE PROGRAM, INCLUDING PURSUANT TO 
-*    SCHEMATICS, INSTRUCTIONS OR RECOMMENDATIONS OF ROBOPLAN, WILL BE SAFE 
-*    FOR PERSONAL USE OR FOR PRODUCTION OR COMMERCIAL USE, WILL NOT 
-*    VIOLATE ANY THIRD PARTY RIGHTS, WILL PROVIDE THE INTENDED OR DESIRED
-*    RESULTS, OR OPERATE AS YOU INTENDED OR AS MAY BE INDICATED BY ROBOPLAN. 
-*    YOU HEREBY WAIVE, AGREE NOT TO ASSERT AGAINST, AND RELEASE ROBOPLAN, 
-*    ITS LICENSORS AND AFFILIATES FROM, ANY CLAIMS IN CONNECTION WITH ANY OF 
-*    THE ABOVE. 
-********************************************************/
